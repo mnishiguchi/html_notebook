39fd66a359bb05757480d46d1c579de6de086973
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-10-21 06:24:51 -0400

    Add mement.js

---
+++ case_problems/Nishiguchi_HamiltonConferenceCenter/javascripts/custom.js
@@ -0,0 +1,30 @@
+/*
+   New Perspectives on HTML
+   Tutorial 5
+   Case Problem 4
+
+   Hamilton Conference Center Style Sheet
+   Author: Masatoshi Nishiguchi
+   Date:   10/14/2015
+
+   Filename:         custom.js
+   Dependencies:     jQuery, moment.js
+*/
+
+// Default date
+var schedulingDate = moment("20170411", "YYYYMMDD");
+
+// Set the default date
+$( document ).ready(function() {
+    $("#scheduling-date").text(schedulingDate.format("dddd, MMMM Do"));
+
+    // The day before button
+   $('#date-before').click(function() {
+      $("#scheduling-date").text(schedulingDate.subtract(1, "days").format("dddd, MMMM Do"));
+   });
+
+   // The day after button
+   $('#date-after').click(function() {
+      $("#scheduling-date").text(schedulingDate.add(1, "days").format("dddd, MMMM Do"));
+   });
+});
