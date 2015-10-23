f64c70f21344bec606dd1db04d36ab5aecda07d9
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-10-23 18:44:17 -0400

    Improve styles

--- case_problems/Nishiguchi_MillenniumComputers/javascripts/custom.js
+++ case_problems/Nishiguchi_MillenniumComputers/javascripts/custom.js
@@ -12,9 +12,22 @@
    Dependencies:     jQuery, moment.js
 */
 
-// Keep on updating the current time
+
 $( document ).ready(function() {
+
+  alert("--- SPECIAL DISCOUNT ---\nIf you order within 10 minutes from now, I will give you 20% discount.");
+
+  // Record visit time
+  var visitTime = moment();
+
+  // Keep on updating the current time
   setInterval(function() {
       $("#time-now").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
   }, 1000);
+
+  // Show how much time has passed
+  setInterval(function() {
+      $("#time-elapsed").text(visitTime.toNow(true) + " passed...");
+  }, 60000);
+
 });
